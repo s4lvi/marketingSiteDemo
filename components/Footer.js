b498ed997d0e5e8dbcffd86ddc90eec276d3e900
s4lvi <salvicomputers@gmail.com>
2022-11-11 23:04:46 -0500

    removed ampli

--- components/Footer.js
+++ components/Footer.js
@@ -1,24 +1,7 @@
 import Image from "next/image"
-import { ampli } from "../src/ampli";
 
 const Footer = () => {
 
-    const opt = () => {
-        window.OneTrust.ToggleInfoDisplay();
-        ampli.track("cookie settings link clicked");
-    }
-
-    const fordLinkClicked = () => {
-        ampli.track("ford.com link clicked");
-    }
-
-    const privacyLinkClicked = () => {
-        ampli.track("privacy link clicked");
-    }
-
-    const termConditionClicked = () => {
-        ampli.track("terms and conditions link clicked");
-    }
 
     return (
         <section id="footer">
@@ -26,9 +9,9 @@ const Footer = () => {
                 <div className={"img"}><Image src={`${process.env.cdn}/assets/footer/logo.svg`} width={139} height={71} layout={"responsive"} alt={"drive logo"} loading={"eager"}/></div>
             </div>
             <div className="footer_links white-text">
-                <div><a href="" onClick={termConditionClicked} target="_blank" rel="noreferrer">Terms & Conditions</a></div>
-                <div><a href="#" onClick={opt}>Cookie Settings</a></div>
-                <div><a href="" target="_blank" rel="noreferrer" onClick={privacyLinkClicked}>Privacy</a></div>
+                <div><a href="" target="_blank" rel="noreferrer">Terms & Conditions</a></div>
+                <div><a href="#" >Cookie Settings</a></div>
+                <div><a href="" target="_blank" rel="noreferrer">Privacy</a></div>
             </div>
             <div className="copy white-text">
                 &copy; Ford Drive. All Rights Reserved.
